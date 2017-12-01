5d2e5b44fb547d6e5adbbc99298cd832450db65d
liambilbo <guillermoalvarez93@gmail.com>
2017-12-01 11:50:23 +0100

    Initial

--- catalog_test.go
+++ catalog_test.go
@@ -70,6 +70,12 @@ func FindBooks(t *testing.T) {
 	numberbooks5:=catalogRepository.CountByText("mongodb -\"second edition\"")
 	assert.Equal(t,1,numberbooks5,"Error CountByText")
 
+	report:=catalogRepository.FindByTextAndStatus("mongodb","MEAP",Page{Size:10,Number:1})
+	assert.Equal(t,1,len(report),"Error FindByTextAndStatus")
+
+
+
+
 }
 
 func ExampleFindBooksByText() {
@@ -77,13 +83,48 @@ func ExampleFindBooksByText() {
 	books:=catalogRepository.FindByText("actions",Page{5,1})
 
 	for _,v :=range books {
-		fmt.Printf("Title : %s\n" , v.Title)
+		fmt.Printf("Title : %s , Score : %f\n" , v.Title, v.Score)
 	}
 	// Output:
-	// Title : SQR in PeopleSoft and Other Applications
-	// Title : Algorithms of the Intelligent Web
-	// Title : Java Persistence with Hibernate
-	// Title : SOA Security
-	// Title : Ext JS in Action, Second Edition
+	//Title : Spring Batch in Action , Score : 11.666667
+	//Title : Hadoop in Action , Score : 9.249910
+	//Title : HTML5 in Action , Score : 9.052156
+	//Title : Jess in Action , Score : 8.780835
+	//Title : MongoDB in Action , Score : 8.772244
 
 }
+
+func ExampleFindByTextAggregation() {
+
+	books:=catalogRepository.FindByTextAggregation2("actions",Page{5,1})
+
+	for _,v :=range books {
+		fmt.Printf("Title : %s , Score : %f\n" , v.Title, v.Score)
+	}
+	// Output:
+	//Title : Spring Batch in Action , Score : 11.666667
+	//Title : Hadoop in Action , Score : 9.249910
+	//Title : HTML5 in Action , Score : 9.052156
+	//Title : Jess in Action , Score : 8.780835
+	//Title : MongoDB in Action , Score : 8.772244
+
+}
+
+func ExampleFindByTextSmartScore() {
+
+	books:=catalogRepository.FindByTextSmartScore("\"mongodb\" in action","english",Page{5,1})
+
+	for _,v :=range books {
+		fmt.Printf("Title : %s , Score : %f , Multiplier : %f , AdjScore : %f\n" , v.Title, v.Score , v.Multiplier, v.AdjScore)
+	}
+	// Output:
+    // Title : MongoDB in Action, Second Edition , Score : 12.500000 , Multiplier : 3.000000 , AdjScore : 0.000000
+	// Title : MongoDB in Action , Score : 18.448653 , Multiplier : 1.000000 , AdjScore : 0.000000
+
+}
+
+
+
+
+
+
--- service/catalogRepository.go
+++ service/catalogRepository.go
@@ -14,10 +14,50 @@ func NewCatalogRepository(c *mgo.Collection) *CatalogRepository {
 	return &CatalogRepository{c}
 }
 
-func (r *CatalogRepository) FindByText(search string,page Page) []struct{Id bson.ObjectId;Title string}{
-	var resp []struct{Id bson.ObjectId;Title string}
+func (r *CatalogRepository) FindByText(search string,page Page) []struct{Id bson.ObjectId;Title string;Score float64}{
+	var resp []struct{Id bson.ObjectId;Title string;Score float64}
 	//err:=r.C.Find(bson.M{"$text":bson.M{"$search":search}}).Skip(page.skip()).Limit(page.limit()).Select(bson.M{"title":1}).All(&resp)
-	err:=r.C.Find(bson.M{"$text":bson.M{"$search":search}}).Skip(page.skip()).Limit(page.limit()).Select(bson.M{"title":1}).All(&resp)
+	err:=r.C.Find(bson.M{"$text":bson.M{"$search":search}}).Skip(page.skip()).Limit(page.limit()).
+		Select(bson.M{"_id":0,"title":1,"score":bson.M{"$meta":"textScore"}}).
+				Sort("$textScore:score").All(&resp)
+
+	if err!=nil {
+		log.Fatalf("Error %s" , err.Error())
+	}
+	return resp
+}
+
+func (r *CatalogRepository) FindByTextAggregation(search string,page Page) []struct{Id bson.ObjectId;Title string;Score float64}{
+	var resp []struct{Id bson.ObjectId;Title string;Score float64}
+
+	err:=r.C.Pipe([]bson.M{
+		bson.M{"$match":bson.M{"$text":bson.M{"$search":search}}},
+		bson.M{"$sort":bson.M{"score":bson.M{"$meta":"textScore"}}},
+		bson.M{"$project":bson.M{"title":1,"score":bson.M{"$meta":"textScore"}}},
+		bson.M{"$skip":page.skip()},
+		bson.M{"$limit":page.limit()},
+		},
+	).All(&resp)
+
+
+	if err!=nil {
+		log.Fatalf("Error %s" , err.Error())
+	}
+	return resp
+}
+
+func (r *CatalogRepository) FindByTextAggregation2(search string,page Page) []struct{Id bson.ObjectId;Title string;Score float64}{
+	var resp []struct{Id bson.ObjectId;Title string;Score float64}
+
+	err:=r.C.Pipe([]bson.M{
+		bson.M{"$match":bson.M{"$text":bson.M{"$search":search}}},
+		bson.M{"$project":bson.M{"title":1,"score":bson.M{"$meta":"textScore"}}},
+		bson.M{"$sort":bson.M{"score":-1}},
+		bson.M{"$skip":page.skip()},
+		bson.M{"$limit":page.limit()},
+	},
+	).All(&resp)
+
 
 	if err!=nil {
 		log.Fatalf("Error %s" , err.Error())
@@ -27,6 +67,49 @@ func (r *CatalogRepository) FindByText(search string,page Page) []struct{Id bson
 
 func (r *CatalogRepository) CountByText(search string) int{
 	resp,err:=r.C.Find(bson.M{"$text":bson.M{"$search":search}}).Count()
+
+	if err!=nil {
+		log.Fatalf("Error %s" , err.Error())
+	}
+	return resp
+}
+
+func (r *CatalogRepository) FindByTextAndStatus(search string,status string,page Page) []struct{Title string;Status string}{
+	var resp []struct{Title string;Status string}
+
+	err:=r.C.Find(bson.M{"$text":bson.M{"$search":search},"status":status}).Skip(page.skip()).Limit(page.limit()).Select(bson.M{"_id":0,"title":1,"status":1}).All(&resp)
+	if err!=nil {
+		log.Fatalf("Error %s" , err.Error())
+	}
+	return resp
+}
+
+func (r *CatalogRepository) FindByTextSmartScore(search string,language string,page Page) []struct{Title string;Score float64;Multiplier float64;AdjScore float64}{
+	var resp []struct{Title string;Score float64;Multiplier float64;AdjScore float64}
+
+	err:=r.C.Pipe([]bson.M{
+		bson.M{"$match":bson.M{"$text":bson.M{"$search":search,"$language":language}}},
+		bson.M{"$project":bson.M{"title":1,
+		                         "score":bson.M{"$meta":"textScore"},
+		"multiplier":bson.M{"$cond":[]interface{}{"$longDescription",1.0,3.0}}}},
+		bson.M{"$project":bson.M{"_id":0,
+		                         "title":1,
+			                     "score":1,
+								  "multiplier":1,
+			                     "adjScore":bson.M{"$multiply":[]interface{}{"$score","$multiplier"}}},
+		                     },
+		bson.M{"$project":bson.M{"_id":0,
+			"title":1,
+			"score":1,
+			"multiplier":1,
+			"adjScore":1,},
+		},
+		bson.M{"$sort":bson.M{"adjScore":-1}},
+		bson.M{"$skip":page.skip()},
+		bson.M{"$limit":page.limit()},
+	},
+	).All(&resp)
+
 
 	if err!=nil {
 		log.Fatalf("Error %s" , err.Error())
